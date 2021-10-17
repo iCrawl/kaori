c368107a3c12e8ebc1c12e22c6c11d9b12c7975c
iCrawl <buechler.noel@outlook.com>
2021-10-18 01:50:53 +0200

    fix: node-fetch typings

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,7 +4,8 @@ import { search } from '../src/index';
 test('should throw on invalid site', async () => {
 	try {
 		await search('asd123');
-	} catch (error) {
+	} catch (e) {
+		const error = e as Error;
 		expect(error).toBeInstanceOf(Error);
 		expect(error.message).toBe('This site is not supported.');
 	}
@@ -14,7 +15,8 @@ test('should throw on non-string', async () => {
 	try {
 		// @ts-ignore
 		await search(123);
-	} catch (error) {
+	} catch (e) {
+		const error = e as Error;
 		expect(error).toBeInstanceOf(Error);
 		expect(error.message).toBe('This site is not supported.');
 	}
@@ -24,7 +26,8 @@ test('should throw on tags not being an array', async () => {
 	try {
 		// @ts-ignore
 		await search('danbooru', { tags: 'test' });
-	} catch (error) {
+	} catch (e) {
+		const error = e as Error;
 		expect(error).toBeInstanceOf(Error);
 		expect(error.message).toBe('Tags have to be an array.');
 	}
@@ -34,7 +37,8 @@ test('should throw on limit not being a number', async () => {
 	try {
 		// @ts-ignore
 		await search('danbooru', { limit: {} });
-	} catch (error) {
+	} catch (e) {
+		const error = e as Error;
 		expect(error).toBeInstanceOf(Error);
 		expect(error.message).toBe('Limit has to be a number.');
 	}
